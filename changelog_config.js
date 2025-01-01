41581a4c92e030985845b89a968de944ad082501
Datumza <reitumetsemasilo@gmail.com>
2025-01-01 20:40:12 +0200

    ci: add changelog config file

---
+++ changelog_config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "types": [
+      { "type": "feat", "section": "Features" },
+      { "type": "fix", "section": "Bug Fixes" },
+      { "type": "docs", "section": "Documentation" },
+      { "type": "style", "section": "Styles", "hidden": true },
+      { "type": "refactor", "section": "Code Refactoring", "hidden": true },
+      { "type": "test", "section": "Tests", "hidden": true },
+      { "type": "chore", "section": "Chores", "hidden": true },
+      { "type": "perf", "section": "Performance Improvements" },
+      { "type": "build", "section": "Builds", "hidden": true },
+      { "type": "ci", "section": "Continuous Integration", "hidden": true }
+    ],
+    "commitUrlFormat": "https://github.com/Contigion/trade-stops-manager/commit/{{hash}}",
+    "compareUrlFormat": "https://github.com/Contigion/trade-stops-manager/compare/{{previousTag}}...{{currentTag}}"
+};
